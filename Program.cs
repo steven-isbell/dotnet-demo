5e0a0bc2d398e944c34dc05bcefd27ed9c989a82
Steven Isbell <steven.isbell18@gmail.com>
2018-02-23 12:23:15 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Integer: " + 10 +
+                " Double: " + 3.14 +
+                " Boolean: " + true);
+
+            // To print without a new line, use Console.Write
+            Console.Write("Hello ");
+            Console.Write("World");
+        }
+    }
+}
